0081546c77b1f4c9af83f56ed304a9d7889da7f8
S0beEk <kapson06@icloud.com>
2023-10-02 19:14:34 +0200

    First commit

---
+++ Project_uno/routes/web.php
@@ -0,0 +1,134 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//require __DIR__.'/auth.php';
+
+Route::get('city_old', function(){
+    return 'Miasto';
+});
+
+Route::get('city', function(){
+    return view('city');
+});
+
+Route::get('info', function(){
+    return view('info', ['firstName' => 'Janusz', 'lastName' => 'Nowak', 'city' => 'Poznan']);
+});
+
+Route::get('info_age/{age}', function($age){
+    return view('info', ['firstName' => 'Janusz', 'lastName' => 'Nowak', 'city' => 'Poznan', 'age' => $age]);
+});
+
+Route::get('pages/{page}', function($page){
+    $pages = [
+        'about' => 'Informacje o stronie',
+        'contact' => 'contact@gmail.com',
+        'home' => 'Strona domowa'
+    ];
+    return $pages[$page];
+});
+
+Route::redirect('miasto', 'city');
+//Route::redirect('/', 'city');
+
+
+Route::get('/address', function(){
+  echo "Miasto: ";
+});
+/*
+Route::get('/address1/{city}', function(string $city){
+  echo "Miasto: $city";
+});
+
+Route::get('/address2/{city}/{street}', function(string $city, string $street){
+  echo <<< ADDRESS
+    Miasto: $city<br>
+    Ulica: $street
+    <hr>
+ADDRESS;
+});
+
+Route::get('/address3/{city}/{street}/{zipCode}', function(string $city, string $street, int $zipCode){
+  $zipCode = substr($zipCode, 0, 2)."-".substr($zipCode, 2, 3);
+  echo <<< ADDRESS
+    Kod pocztowy: $zipCode<br>
+    Miasto: $city<br>
+    Ulica: $street
+    <hr>
+ADDRESS;
+});*/
+
+
+Route::get('/address4/{city?}/{street?}/{zipCode?}', function(string $city = "-", string $street = "-", int $zipCode = null){
+  if (is_null($zipCode))
+    $zipCode = "brak danych";
+  else
+    $zipCode = substr($zipCode, 0, 2)."-".substr($zipCode, 2, 3);
+  echo <<< ADDRESS
+    Kod pocztowy: $zipCode<br>
+    Miasto: $city<br>
+    Ulica: $street
+    <hr>
+ADDRESS;
+});
+
+Route::get('/address5/{city?}/{street?}/{zipCode?}', function(string $city = "-", string $street = "-", int $zipCode = null){
+  $zipCode = is_null($zipCode) ? "brak danych!" : substr($zipCode, 0, 2)."-".substr($zipCode, 2, 3);
+
+  echo <<< ADDRESS
+    Kod pocztowy: $zipCode<br>
+    Miasto: $city<br>
+    Ulica: $street
+    <hr>
+ADDRESS;
+})->name('x');
+
+Route::redirect('/adres/{city?}/{street?}/{zipCode?}', '/address5/{city?}/{street?}/{zipCode?}');
+
+
+
+Route::get('/address6/{city?}/{street?}/{zipCode?}/{phone_number?}',function(string $city = "-", string $street = "-", int $zipCode = null, $phone_number)
+{
+  if(is_null($zipCode))
+  
+    $zipCode = "brak dancy";
+  
+  else
+
+    $zipCode = substr($zipCode, 0, 2)."-".substr($zipCode, 2, 3);
+
+      echo <<< ADRES
+        Miasto: $city<br>
+        Ulica: $street<br>
+        Kod pocztowy: $zipCode<br>
+        Numer telefonu: $phone_number<br> 
+      ADRES;
+  
+});
